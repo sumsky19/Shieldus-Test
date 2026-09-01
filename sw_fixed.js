d30812a690be2eb2c6a0e257110a7b2c9f16ac59
sumsky19 <sumsky19@gmail.com>
2026-09-01 16:47:59 +0900

    Add files via upload

---
+++ sw_fixed.js
@@ -0,0 +1,17 @@
+const CACHE = 'skshieldus-mock-v8-local-3';
+const CORE = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./skshieldus-logo.png'];
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE).catch(()=>{})));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).then(response => {
+    const copy=response.clone();
+    caches.open(CACHE).then(c=>c.put(event.request,copy)).catch(()=>{});
+    return response;
+  }).catch(()=>caches.match(event.request)));
+});
